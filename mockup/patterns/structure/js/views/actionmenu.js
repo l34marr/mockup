f76b4f7a18306b7fd9ccfb8a1d7b368cfe90055c
Johannes Raggam <thetetet@gmail.com>
2016-07-06 13:36:53 +0200

    Structure pattern actionmenu: ``patternClasses`` option on actionmenu items for initializing extra patterns.

--- mockup/patterns/structure/js/views/actionmenu.js
+++ mockup/patterns/structure/js/views/actionmenu.js
@@ -9,7 +9,6 @@ define([
   'text!mockup-patterns-structure-url/templates/actionmenu.xml',
   'pat-registry',
   'translate',
-  'mockup-patterns-modal',
   'mockup-patterns-tooltip',
   'bootstrap-dropdown'
 ], function($, _, BaseView, utils, Result, Actions, ActionMenu, ActionMenuTemplate, registry, _t) {
@@ -23,7 +22,6 @@ define([
     menuOptions: null,
     // Dynamic menu options
     menuGenerator: 'mockup-patterns-structure-url/js/actionmenu',
-    needsRescan: false,
 
     eventConstructor: function(definition) {
       var self = this;
@@ -60,11 +58,17 @@ define([
               menuOptionsCategorized[category] = [];
           }
           menuOptionsCategorized[category].push(menuOption);
-          if (menuOption.modal || menuOption.category === 'button') {
-            self.needsRescan = true;
+          if (!menuOption.patternClasses) {
+            // set default to empty string
+            menuOption.patternClasses = '';
+          }
+          if (menuOption.modal === true) {
+            // add standard pat-plone-modal.
+            // If you want another modal implementation, don't use modal=true but only set patternClasses.
+            menuOption.patternClasses += ' pat-plone-modal';
           }
 
-		      // Create event handler and add it to the results object.
+          // Create event handler and add it to the results object.
           var e = self.eventConstructor(menuOption);
           if (e) {
             result['click a.' + idx] = e;
@@ -103,7 +107,6 @@ define([
       var data = this.model.toJSON();
       data.header = self.options.header || null;
       data.menuOptions = self.menuOptionsCategorized;
-
       self.$el.html(self.template($.extend({
         _t: _t,
         id: utils.generateId()
@@ -118,9 +121,7 @@ define([
         self.$el.addClass(self.options.className);
       }
 
-      if (this.needsRescan) {
-        registry.scan(this.$el);
-      }
+      registry.scan(this.$el);
 
       return this;
     }
